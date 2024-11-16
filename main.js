1efbe18d00544e9fbb2b6a0658154ce673bca304
pakulov_de <pakulov_de@devtrix.ru>
2024-11-16 21:46:36 +0500

    deprecated bug

--- main.js
+++ main.js
@@ -1,46 +1,23 @@
-document.getElementById("status").innerHTML =
-  "DeviceOrientationEvent: " +
-  (window.DeviceOrientationEvent ? "" : "not ") +
-  "supported<br>";
-document.getElementById("status").innerHTML +=
-  "DeviceMotionEvent: " +
-  (window.DeviceMotionEvent ? "" : "not ") +
-  "supported<br>";
-document.getElementById("status").innerHTML +=
-  "MozOrientation: " +
-  (window.onMozOrientation ? "" : "not ") +
-  "supported<br>";
+window.addEventListener("devicemotion", (event) => {
+  var status = document.getElementById("status");
+  status.innerText =
+    "devicemotion: acceleration.x=" +
+    event.acceleration.x +
+    ", acceleration.y=" +
+    event.acceleration.y +
+    ", acceleration.z=" +
+    event.acceleration.z +
+    ", interval=" +
+    event.interval;
+});
 
-if (window.DeviceOrientationEvent) {
-  window.addEventListener(
-    "deviceorientation",
-    (event) => {
-      document.getElementById("status").innerHTML =
-        "beta: " + event.beta + "<br>gamma: " + event.gamma;
-    },
-    true
-  );
-} else if (window.DeviceMotionEvent) {
-  window.addEventListener(
-    "devicemotion",
-    (event) => {
-      document.getElementById("status").innerHTML =
-        "acceleration: x=" +
-        event.acceleration.x +
-        ", y=" +
-        event.acceleration.y +
-        ", z=" +
-        event.acceleration.z;
-    },
-    true
-  );
-} else {
-  window.addEventListener(
-    "MozOrientation",
-    (event) => {
-      document.getElementById("status").innerHTML =
-        "x: " + event.x + "<br>y: " + event.y;
-    },
-    true
-  );
-}
+window.addEventListener("deviceorientation", (event) => {
+  var status = document.getElementById("status");
+  status.innerText =
+    "deviceorientation: alpha=" +
+    event.alpha +
+    ", beta=" +
+    event.beta +
+    ", gamma=" +
+    event.gamma;
+});
